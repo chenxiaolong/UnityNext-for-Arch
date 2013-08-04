fbd17ab5925b990cfe8a66755864c737c22c0e08
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-08-04 16:07:39 -0400

    Remove more useless stuff

--- ubuntu-touch-meta/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ..."
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} ${1:-saucy} native)"
