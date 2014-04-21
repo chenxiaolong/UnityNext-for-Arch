1cf02acf292732787aa4574ea950beddc7e303e4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-21 16:52:58 -0400

    [xf86-video-intel-mir] Update to version 2.99.911 with Ubuntu version 2.99.910 and release 0ubuntu1

--- xf86-video-intel-mir/compare_versions.sh
+++ xf86-video-intel-mir/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   (write me a command for this)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version xserver-xorg-video-intel ${1:-saucy})"
+echo -e "Ubuntu version:     $(get_ubuntu_version xserver-xorg-video-intel ${1:-trusty})"
